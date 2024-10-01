50dab15586e60b78bd2bbc11591731424ff589bc
JOSE WILK FERREIRA DE MELO <wilk.jose.sb@gmail.com>
2024-10-01 18:14:47 -0300

    Corrigindo ex016.

--- cursojs/aula14ex/ex016/script.js
+++ cursojs/aula14ex/ex016/script.js
@@ -1,18 +1,32 @@
-function conta(){
+function contar(){
 let ini = document.getElementById('txti')
 let fim = document.getElementById('txtf')
 let passo = document.getElementById('txtp')
 let res = document.getElementById('res')
 
-if (ini.value.length == 0 || fim.value.length == 0 || passo.valeu.length == 0) {
+if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+    res.innerHTML = 'Impossível contar!'
     window.alert('[ERRO] faltam dados!')
 } else {
-    res.innerHTML = 'Contando:'
+    res.innerHTML = 'Contando: <br>'
     let i = Number(ini.value)
     let f = Number(fim.value)
-    let p =Number(passo.value)
-
-    for(let c = i; c <= f; c += p ) 
-        res.innerHTML += `${c} \u{1F603}`
-}
+    let p = Number(passo.value)
+    if (p <= 0){
+        window.alert('Passo inválido! Considerando PASSO 1')
+        p = 1
+    }
+    if (i < f) {
+        // Contagem crescente
+        for(let c = i; c <= f; c += p ) {
+        res.innerHTML += `${c} \u{1F449}`
+        }
+    } else {
+        // Contagem regressiva
+        for(let c = i; c >= f; c -= p) {
+             res.innerHTML += `${c} \u{1F449}`
+        }
+    }
+        res.innerHTML =+ `${c} \u{1F3C1}`
+    }
 }
