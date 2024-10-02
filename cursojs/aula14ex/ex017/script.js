cbee938a10cbf84a4056966d676541a0d861c115
JOSE WILK FERREIRA DE MELO <wilk.jose.sb@gmail.com>
2024-10-02 15:51:45 -0300

    sript.js do ex017.

---
+++ cursojs/aula14ex/ex017/script.js
@@ -0,0 +1,9 @@
+function tabuada(){
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+    if(num.value.length == 0) {
+    window.alert('Por favor digite um número!')
+    } else {
+        let n = Number(num.value) 
+    }
+}
