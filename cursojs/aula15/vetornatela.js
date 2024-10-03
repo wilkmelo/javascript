f96e714b33a335c1873474fa8b3bc1b7746f5322
JOSE WILK FERREIRA DE MELO <wilk.jose.sb@gmail.com>
2024-10-03 10:26:12 -0300

    Atualizei o script.

--- cursojs/aula15/vetornatela.js
+++ cursojs/aula15/vetornatela.js
@@ -1,5 +1,5 @@
 let valores = [8, 1, 7, 4, 2, 9]
-
+valores.sort()
 //console.log(valores)
 /*
 console.log(valores[0])
@@ -17,4 +17,3 @@ for( let pos=0; pos < valores.length; pos++) {
 for(let pos in valores){
     console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
 }
-    
